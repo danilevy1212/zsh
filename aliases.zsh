8dfb81679b1dc6c41f295181670a33157fb93d32
dlevym <danilevy1212>
2020-07-06 22:51:28 +0200

    Very very early beginnings of vscode integration.

--- aliases.zsh
+++ aliases.zsh
@@ -8,11 +8,14 @@ alias ek="emacsclient -e '(kill-emacs)'"
 # create a terminal emacs
 alias emt="emacs -nw"
 
+# vs-code FIXME This make more sense to define as some sort of function instead, and put it in .local/bin
+alias vsc="code --extensions-dir $XDG_CONFIG_HOME/Code\ -\ OSS/extensions"
+
 # nvim
-if [ `command -v nvim` ]; then 
+if [ `command -v nvim` ]; then
     alias vim="nvim"
     alias gvim="nvim-qt"
 fi
 
-[ -f "$XDG_CONFIG_HOME/zsh/secret_aliases.zsh" ] && 
+[ -f "$XDG_CONFIG_HOME/zsh/secret_aliases.zsh" ] &&
   source "$XDG_CONFIG_HOME/zsh/secret_aliases.zsh"
